5fe5863c317ed54bf9cd41bcc59b6b5a2f0331af
Gabriel Aguiar <gamarques@rd.com.br>
2023-12-08 09:54:35 -0300

    feat: use DI container provider

--- App.tsx
+++ App.tsx
@@ -1,29 +1,27 @@
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-} from 'react-native';
+import {SafeAreaView, StatusBar, StyleSheet} from 'react-native';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+import HomeScreen from './src/modules/task/ui/screens/HomeScreen';
+import {Provider} from './src/global/context/Context';
+import {DIContainerImpl} from './src/core/di/impl/DIContainerImpl';
 
 function App(): JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const container = DIContainerImpl.getDefault();
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-    </SafeAreaView>
+    <Provider container={container}>
+      <SafeAreaView style={styles.container}>
+        <StatusBar barStyle={'dark-content'} />
+        <HomeScreen />
+      </SafeAreaView>
+    </Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
